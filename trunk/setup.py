a57ac8cd55cfe2d5825cfec2e52ca466e431b9e5
simon.drabble <simon.drabble@fc4e396d-3446-0410-94af-5b4516e3465f>
2008-02-18 17:03:47 +0000

    Added setup.py

---
+++ trunk/setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python2.4
+
+
+__author__ = 'Simon Drabble <python-devel@thebigmachine.org>'
+
+
+from distutils.core import setup
+
+
+setup(name    = 'riff',
+      version = '0.1a',
+      author  = 'Simon Drabble',
+      author_email = 'python-devel@thebigmachine.org',
+      url     = 'http://code.google.com/p/pyriff/',
+      scripts  = [],
+      packages = ['riff'],
+      license = 'GPL2',
+      description = 'RIFF manipulation library',
+    )
+
